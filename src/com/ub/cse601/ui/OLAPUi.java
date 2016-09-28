d2153bb4eebf22427899eeef8affcfed5b3d7047
ramcode <ramesh.thetakali@gmail.com>
2016-09-28 17:03:32 -0400

    OlapUi changes

--- src/com/ub/cse601/ui/OLAPUi.java
+++ src/com/ub/cse601/ui/OLAPUi.java
@@ -1,23 +1,6 @@
 package com.ub.cse601.ui;
 
-import java.awt.Cursor;
 import java.awt.EventQueue;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-import java.sql.Connection;
-import java.sql.ResultSet;
-
-import javax.swing.BoxLayout;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.LayoutStyle;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
-
-import com.ub.cse601.sql.DBClient;
 
 public class OLAPUi {
 	private static final long serialVersionUID = 1L;
