b9be66fde3d84369c369e2d3a52f0c34a4fffad3
ramcode <ramesh.thetakali@gmail.com>
2016-09-28 16:58:35 -0400

    query changes

--- src/com/ub/cse601/sql/OLAPQueries.java
+++ src/com/ub/cse601/sql/OLAPQueries.java
@@ -10,214 +10,203 @@ import java.util.Map;
 
 public class OLAPQueries {
 
+	public DBClient dbClient;
+	public Connection conn;
 
-    public DBClient dbClient;
-    public Connection conn;
+	public OLAPQueries() {
 
-    public OLAPQueries() {
-
-        dbClient = new DBClient();
-        conn = dbClient.getConn();
-
-    }
-
-    public int queryForTumorALLPatients ( String description, String type, String[] name ) throws SQLException {
-
-        Statement stmt = null;
-        int count = 0;
-
-        try {
-
-            String query = "select * from clinical_fact, disease where clinical_fact.DS_ID=disease.DS_ID AND disease.name='ALL' and disease.type='leukemia' and disease.description='tumor'";
-
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-
-                count++;
-
-            }
-
-
-        } catch ( SQLException e ) {
-
-            printSQLException(e);
-
-        } finally {
-
-            if ( stmt != null ) {
-
-                stmt.close();
-
-            }
-
-        }
-        return count;
-
-    }
-
-    public ArrayList<String> drugsForPatientsWithTumor( String description ) throws SQLException {
-
-        ArrayList<String> drugList = new ArrayList<String>();
-
-        Statement stmt = null;
-
-        try {
-
-            String query = "select drug.type,disease.DESCRIPTION from drug, clinical_fact,disease where clinical_fact.DR_ID=drug.DR_ID AND clinical_fact.DS_ID=disease.DS_ID AND disease.DESCRIPTION='tumor'";
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-
-                String drugType = rs.getString("TYPE");
-                String[] drugData = drugType.split(" ");
-                drugList.add(drugData[2]);
-
-            }
-
-
-        } catch ( SQLException e ) {
-
-            printSQLException(e);
-
-        } finally {
-
-            if ( stmt != null ) {
-
-                stmt.close();
-
-            }
-
-        }
-
-        return drugList;
-
-    }
-
-
-    public Map<String,String> allDiseaseList() throws SQLException {
-
-        Map<String,String> diseaseMap = new HashMap<String, String>();
-
-        Statement stmt = null;
-
-        try {
-
-            String query = "select disease.NAME, disease.DS_ID from disease";
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-
-                String name = rs.getString("NAME");
-                String id = ""+ rs.getInt("DS_ID");
-                diseaseMap.put(id, name);
-                
-            }
-
-
-        } catch ( SQLException e ) {
-
-            printSQLException(e);
-
-        } finally {
-
-            if ( stmt != null ) {
-
-                stmt.close();
-
-            }
-
-        }
-
-        return diseaseMap;
-
-    }
-
-
-	
-	/*public static void main(String[] args){
-
-        Statement stmt = null;
-
-        String query = "select * from clinical_fact, disease where clinical_fact.DS_ID=disease.DS_ID AND disease.name='ALL' and disease.type='leukemia' and disease.description='tumor'";
-
-        try {
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            int count = 0;
-
-            while (rs.next()) {
-
-                count++;
-
-            }
-
-            System.out.println("Count:" + count);
-
-        } catch (SQLException e ) {
-            e.printStackTrace();
-        } *//*finally {
-            if (stmt != null) {
-                stmt.close(); }
-        }*//*
-
-
-
+		dbClient = new DBClient();
+		conn = dbClient.getConn();
 
 	}
-    */
 
-    // Helper functions -- referenced from oracle Website
+	public int queryForTumorALLPatients(String description, String type, String[] name) throws SQLException {
 
-    public static void printSQLException(SQLException ex) {
+		Statement stmt = null;
+		int count = 0;
 
-        for ( Throwable e : ex ) {
+		try {
 
-            if ( e instanceof SQLException ) {
+			String query = "select * from clinical_fact, disease where clinical_fact.DS_ID=disease.DS_ID AND disease.name='ALL' and disease.type='leukemia' and disease.description='tumor'";
 
-                if ( ignoreSQLException (
-                        ( (SQLException )e ).
-                                getSQLState() ) == false ) {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
 
-                    e.printStackTrace(System.err);
-                    System.err.println("SQLState: " +
-                            ((SQLException)e).getSQLState());
+			while (rs.next()) {
 
-                    System.err.println("Error Code: " +
-                            ((SQLException)e).getErrorCode());
+				count++;
 
-                    System.err.println("Message: " + e.getMessage());
+			}
 
-                    Throwable t = ex.getCause();
-                    while(t != null) {
-                        System.out.println("Cause: " + t);
-                        t = t.getCause();
-                    }
-                }
-            }
-        }
-    }
+		} catch (SQLException e) {
 
-    public static boolean ignoreSQLException(String sqlState) {
+			printSQLException(e);
 
-        if (sqlState == null) {
-            System.out.println("The SQL state is not defined!");
-            return false;
-        }
+		} finally {
 
-        // X0Y32: Jar file already exists in schema
-        if (sqlState.equalsIgnoreCase("X0Y32"))
-            return true;
+			if (stmt != null) {
 
-        // 42Y55: Table already exists in schema
-        if (sqlState.equalsIgnoreCase("42Y55"))
-            return true;
+				stmt.close();
 
-        return false;
-    }
-	
+			}
+
+		}
+		return count;
+
+	}
+
+	public ArrayList<String> drugsForPatientsWithTumor(String description) throws SQLException {
+
+		ArrayList<String> drugList = new ArrayList<String>();
+
+		Statement stmt = null;
+
+		try {
+
+			String query = "select drug.type,disease.DESCRIPTION from drug, clinical_fact,disease where clinical_fact.DR_ID=drug.DR_ID AND clinical_fact.DS_ID=disease.DS_ID AND disease.DESCRIPTION='tumor'";
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+
+				String drugType = rs.getString("TYPE");
+				String[] drugData = drugType.split(" ");
+				drugList.add(drugData[2]);
+
+			}
+
+		} catch (SQLException e) {
+
+			printSQLException(e);
+
+		} finally {
+
+			if (stmt != null) {
+
+				stmt.close();
+
+			}
+
+		}
+
+		return drugList;
+
+	}
+
+	public Map<Integer, String> allDiseaseList() throws SQLException {
+
+		Map<Integer, String> diseaseMap = new HashMap<Integer, String>();
+
+		Statement stmt = null;
+
+		try {
+
+			String query = "select disease.NAME, disease.DS_ID from disease";
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+
+				String name = rs.getString("NAME");
+				Integer id = rs.getInt("DS_ID");
+				diseaseMap.put(id, name);
+
+			}
+
+		} catch (SQLException e) {
+
+			printSQLException(e);
+
+		} finally {
+
+			if (stmt != null) {
+
+				stmt.close();
+
+			}
+
+		}
+
+		return diseaseMap;
+
+	}
+
+	/*
+	 * public static void main(String[] args){
+	 * 
+	 * Statement stmt = null;
+	 * 
+	 * String query =
+	 * "select * from clinical_fact, disease where clinical_fact.DS_ID=disease.DS_ID AND disease.name='ALL' and disease.type='leukemia' and disease.description='tumor'"
+	 * ;
+	 * 
+	 * try { stmt = conn.createStatement(); ResultSet rs =
+	 * stmt.executeQuery(query);
+	 * 
+	 * int count = 0;
+	 * 
+	 * while (rs.next()) {
+	 * 
+	 * count++;
+	 * 
+	 * }
+	 * 
+	 * System.out.println("Count:" + count);
+	 * 
+	 * } catch (SQLException e ) { e.printStackTrace(); }
+	 *//*
+		 * finally { if (stmt != null) { stmt.close(); } }
+		 *//*
+		 * 
+		 * 
+		 * 
+		 * 
+		 * }
+		 */
+
+	// Helper functions -- referenced from oracle Website
+
+	public static void printSQLException(SQLException ex) {
+
+		for (Throwable e : ex) {
+
+			if (e instanceof SQLException) {
+
+				if (ignoreSQLException(((SQLException) e).getSQLState()) == false) {
+
+					e.printStackTrace(System.err);
+					System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+
+					System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+
+					System.err.println("Message: " + e.getMessage());
+
+					Throwable t = ex.getCause();
+					while (t != null) {
+						System.out.println("Cause: " + t);
+						t = t.getCause();
+					}
+				}
+			}
+		}
+	}
+
+	public static boolean ignoreSQLException(String sqlState) {
+
+		if (sqlState == null) {
+			System.out.println("The SQL state is not defined!");
+			return false;
+		}
+
+		// X0Y32: Jar file already exists in schema
+		if (sqlState.equalsIgnoreCase("X0Y32"))
+			return true;
+
+		// 42Y55: Table already exists in schema
+		if (sqlState.equalsIgnoreCase("42Y55"))
+			return true;
+
+		return false;
+	}
+
 }
--- src/com/ub/cse601/ui/CentralBiostarWindow.java
+++ src/com/ub/cse601/ui/CentralBiostarWindow.java
@@ -1,8 +1,9 @@
 package com.ub.cse601.ui;
 
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
@@ -16,6 +17,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
+
+import com.ub.cse601.sql.OLAPQueries;
 
 public class CentralBiostarWindow {
 
@@ -35,12 +38,22 @@ public class CentralBiostarWindow {
 	private JLabel goIdLabel;
 	private JButton resetBtn;
 	private JButton executeBtn;
+	private JLabel msgBox;
+	private OLAPQueries olapQueryClient;
 
 	/**
 	 * Create the application.
 	 */
 	public CentralBiostarWindow() {
-		initialize();
+		this.msgBox = new JLabel("Initializing app....");
+		try {
+			olapQueryClient = new OLAPQueries();
+			initialize();
+			this.msgBox = new JLabel("App initialized...Execute Queries Now");
+
+		} catch (Exception ex) {
+			msgBox.setText("Exception: " + ex.getMessage());
+		}
 	}
 
 	/**
@@ -56,7 +69,7 @@ public class CentralBiostarWindow {
 		probeId.setText("");
 	}
 
-	private void initialize() {
+	private void initialize() throws Exception {
 		frame = new JFrame();
 		frame.setSize(1200, 800);
 		// frame.setBounds(100, 100, 450, 300);
@@ -72,6 +85,8 @@ public class CentralBiostarWindow {
 
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Part 2", null, panel_1, null);
+
+		msgBox = new JLabel("");
 
 		queryLabel = new JLabel("Query");
 
@@ -115,37 +130,67 @@ public class CentralBiostarWindow {
 
 		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING).addGroup(gl_panel_1
-				.createSequentialGroup().addGap(59)
-				.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 849, GroupLayout.PREFERRED_SIZE).addGroup(
-								gl_panel_1.createSequentialGroup()
-										.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-												.addComponent(queryLabel).addComponent(diseaseLabel)
-												.addComponent(clstrLabel).addComponent(probeLabel)
-												.addComponent(mUnitLabel)
-												.addComponent(goIdLabel, GroupLayout.PREFERRED_SIZE, 81,
-														GroupLayout.PREFERRED_SIZE))
-										.addGap(96)
-										.addGroup(
-												gl_panel_1.createParallelGroup(Alignment.LEADING)
-														.addComponent(goId, GroupLayout.DEFAULT_SIZE, 209,
-																Short.MAX_VALUE)
-														.addComponent(probeId, 209, 209, Short.MAX_VALUE)
-														.addComponent(clusterId, 209, 209, Short.MAX_VALUE)
-														.addComponent(queryCombo, 0, 209, Short.MAX_VALUE)
-														.addComponent(diseaseCombo, 0, 209, Short.MAX_VALUE)
-														.addGroup(gl_panel_1.createSequentialGroup()
-																.addComponent(resetBtn, GroupLayout.PREFERRED_SIZE, 96,
-																		GroupLayout.PREFERRED_SIZE)
-																.addPreferredGap(ComponentPlacement.RELATED,
+		gl_panel_1
+				.setHorizontalGroup(
+						gl_panel_1.createParallelGroup(Alignment.LEADING)
+								.addGroup(
+										gl_panel_1
+												.createSequentialGroup().addGap(59).addGroup(gl_panel_1
+														.createParallelGroup(Alignment.LEADING).addGroup(
+																gl_panel_1
+																		.createSequentialGroup()
+																		.addGroup(gl_panel_1
+																				.createParallelGroup(Alignment.LEADING)
+																				.addComponent(queryLabel)
+																				.addComponent(diseaseLabel)
+																				.addComponent(clstrLabel)
+																				.addComponent(probeLabel)
+																				.addComponent(mUnitLabel).addComponent(
+																						goIdLabel,
+																						GroupLayout.PREFERRED_SIZE, 81,
+																						GroupLayout.PREFERRED_SIZE))
+																		.addGap(96)
+																		.addGroup(gl_panel_1
+																				.createParallelGroup(Alignment.LEADING)
+																				.addComponent(goId,
+																						GroupLayout.DEFAULT_SIZE, 209,
+																						Short.MAX_VALUE)
+																				.addComponent(probeId, 209, 209,
+																						Short.MAX_VALUE)
+																				.addComponent(clusterId, 209, 209,
+																						Short.MAX_VALUE)
+																				.addComponent(
+																						queryCombo, 0, 209,
+																						Short.MAX_VALUE)
+																				.addComponent(diseaseCombo, 0,
+																						209, Short.MAX_VALUE)
+																				.addGroup(gl_panel_1
+																						.createSequentialGroup()
+																						.addComponent(resetBtn,
+																								GroupLayout.PREFERRED_SIZE,
+																								96,
+																								GroupLayout.PREFERRED_SIZE)
+																						.addPreferredGap(
+																								ComponentPlacement.RELATED,
+																								GroupLayout.DEFAULT_SIZE,
+																								Short.MAX_VALUE)
+																						.addComponent(executeBtn,
+																								GroupLayout.PREFERRED_SIZE,
+																								106,
+																								GroupLayout.PREFERRED_SIZE))
+																				.addComponent(mesureUnitId,
+																						GroupLayout.DEFAULT_SIZE, 209,
+																						Short.MAX_VALUE))
+																		.addGap(837))
+														.addGroup(gl_panel_1
+																.createParallelGroup(Alignment.TRAILING, false)
+																.addComponent(msgBox, Alignment.LEADING,
+																		GroupLayout.DEFAULT_SIZE,
 																		GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-																.addComponent(executeBtn, GroupLayout.PREFERRED_SIZE,
-																		106, GroupLayout.PREFERRED_SIZE))
-														.addComponent(mesureUnitId, GroupLayout.DEFAULT_SIZE, 209,
-																Short.MAX_VALUE))
-										.addGap(837)))
-				.addContainerGap()));
+																.addComponent(scrollPane, Alignment.LEADING,
+																		GroupLayout.DEFAULT_SIZE, 849,
+																		Short.MAX_VALUE)))
+												.addContainerGap()));
 		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING).addGroup(gl_panel_1
 				.createSequentialGroup().addGap(37)
 				.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE).addComponent(queryLabel).addComponent(
@@ -174,7 +219,7 @@ public class CentralBiostarWindow {
 										.addComponent(executeBtn)))
 						.addComponent(goIdLabel))
 				.addGap(40).addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 268, GroupLayout.PREFERRED_SIZE)
-				.addContainerGap(156, Short.MAX_VALUE)));
+				.addGap(84).addComponent(msgBox).addContainerGap(61, Short.MAX_VALUE)));
 
 		txtQueryOutput = new JTextField();
 		txtQueryOutput.setText("Query Output");
@@ -195,8 +240,23 @@ public class CentralBiostarWindow {
 						.addContainerGap().addComponent(lblDisease_1).addContainerGap(194, Short.MAX_VALUE)));
 		panel_2.setLayout(gl_panel_2);
 
+		// initialize combo boxes
+		queryCombo.insertItemAt("Query 1", 0);
+		queryCombo.insertItemAt("Query 2", 2);
+		queryCombo.insertItemAt("Query 3", 3);
+		queryCombo.insertItemAt("Query 4", 4);
+		queryCombo.insertItemAt("Query 5", 5);
+		queryCombo.insertItemAt("Query 6", 6);
+		diseaseCombo.insertItemAt("All", 0);
+
+		Iterator<Entry<Integer, String>> diseaseMapItr = olapQueryClient.allDiseaseList().entrySet().iterator();
+		while (diseaseMapItr.hasNext()) {
+			Entry<Integer, String> entry = diseaseMapItr.next();
+			diseaseCombo.insertItemAt(entry.getValue(), entry.getKey());
+
+		}
+
 		// show frame
 		this.frame.setVisible(true);
 	}
-
 }
